9ff2f357935b1a0323a6651f6d84fe02a1147c84
sulga29 <sulga29@naver.com>
2018-07-16 23:22:50 +0900

    변수

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int n1 = 8;
+        int n2 = 9;
+        int answer;
+        answer = n1 + n2;
+        Debug.Log(answer);
+
+	}
+}
